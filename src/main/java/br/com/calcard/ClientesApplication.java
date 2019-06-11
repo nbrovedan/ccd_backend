81f29d0eee91141759b3e65f933b7a0b3065f68f
Naidion Concencio Brovedan <brovedan@gmail.com>
2019-06-11 20:06:57 -0300

    Documentacao swagger

--- src/main/java/br/com/calcard/ClientesApplication.java
+++ src/main/java/br/com/calcard/ClientesApplication.java
@@ -2,12 +2,14 @@ package br.com.calcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
 public class ClientesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesApplication.class, args);
 	}
-
 }
+ 
--- src/main/java/br/com/calcard/controllers/ClientesController.java
+++ src/main/java/br/com/calcard/controllers/ClientesController.java
@@ -22,10 +22,16 @@ import br.com.calcard.entities.Clientes;
 import br.com.calcard.entities.Propostas;
 import br.com.calcard.services.ClientesService;
 import br.com.calcard.services.PropostasService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import javassist.NotFoundException;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/clientes")
+@Api(value="Clientes", tags={"Clientes"}, description="Controle de usuários")
 public class ClientesController {
 	
 	@Autowired
@@ -33,47 +39,182 @@ public class ClientesController {
 	@Autowired
 	private PropostasService propostaService; 
 	
+	@ApiOperation(
+		value="Buscar todos os clientes",
+		response=List.class,
+		notes="Retorna todos os clientes cadastrados")
+
+	@ApiResponses(value= {
+		@ApiResponse(
+			code=200,
+			message="Retorna um imóvel",
+			response=List.class),
+		@ApiResponse(
+			code=404,
+			message="Retorna um erro 404 caso nenhum cliente seja encontrado",
+			response=NotFoundException.class)
+		})
 	@GetMapping
 	public List<Clientes> getClientes() {
 		return clientesService.getClientes();
 	}
 	
+	@ApiOperation(
+			value="Buscar cliente pelo código",
+			response=Clientes.class,
+			notes="Retorna o cliente de acordo com o código")
+
+	@ApiResponses(value= {
+		@ApiResponse(
+			code=200,
+			message="Retorna um cliente",
+			response=Clientes.class),
+		@ApiResponse(
+			code=404,
+			message="Retorna um erro 404 caso nenhum cliente seja encontrado",
+			response=NotFoundException.class)
+		})
 	@GetMapping("/{codigo}")
 	public Clientes getCliente(@PathVariable Integer codigo) {
 		return clientesService.getCliente(codigo);
 	}
 	
+	@ApiOperation(
+			value="Buscar cliente pelo CPF",
+			response=Clientes.class,
+			notes="Retorna o cliente de acordo com o CPF")
+
+	@ApiResponses(value= {
+		@ApiResponse(
+			code=200,
+			message="Retorna um cliente",
+			response=Clientes.class),
+		@ApiResponse(
+			code=404,
+			message="Retorna um erro 404 caso nenhum cliente seja encontrado",
+			response=NotFoundException.class)
+		})
 	@GetMapping(params="cpf")
 	public Clientes getCliente(@RequestParam String cpf) {
 		return clientesService.getClienteByCpf(cpf);
 	}
 	
+	@ApiOperation(
+			value="Adicionar um novo cliente",
+			response=Clientes.class,
+			notes="Adiciona um novo cliente e retorna o mesmo")
+
+	@ApiResponses(value= {
+		@ApiResponse(
+			code=200,
+			message="Retorna o cliente inserido",
+			response=Clientes.class),
+		@ApiResponse(
+			code=500,
+			message="Retorna um erro 500 caso algum erro seja encontrado",
+			response=InternalError.class)
+		})
 	@PostMapping
 	public Clientes newCliente(@Valid @RequestBody Clientes cliente) {
 		return clientesService.saveCliente(cliente);
 	}
 	
+	@ApiOperation(
+			value="Atualiza um cliente",
+			response=Clientes.class,
+			notes="Adiciona um cliente de acordo com o código passado")
+
+	@ApiResponses(value= {
+		@ApiResponse(
+			code=200,
+			message="Retorna o cliente atualizado",
+			response=Clientes.class),
+		@ApiResponse(
+			code=500,
+			message="Retorna um erro 500 caso algum erro seja encontrado",
+			response=InternalError.class)
+		})
 	@PutMapping("/{codigo}")
 	public Clientes updateCliente(@PathVariable Integer codigo, @RequestBody Clientes cliente) {
 		return clientesService.updateCliente(codigo, cliente);
 	}
 	
+	@ApiOperation(
+			value="Remove um cliente",
+			response=Clientes.class,
+			notes="Remove um cliente de acordo com o código passado")
+
+	@ApiResponses(value= {
+		@ApiResponse(
+			code=200,
+			message="Retorna uma mensagem de sucesso",
+			response=ResponseEntity.class),
+		@ApiResponse(
+			code=500,
+			message="Retorna um erro 500 caso algum erro seja encontrado",
+			response=InternalError.class)
+		})
 	@DeleteMapping(value = "/{codigo}", produces="application/json")
 	@ResponseBody
 	public ResponseEntity<String> deleteCliente(@PathVariable Integer codigo) {
 		return clientesService.deleteClientes(codigo);
 	}
 	
+	@ApiOperation(
+			value="Adiciona uma proposta",
+			response=Propostas.class,
+			notes="Adiciona uma proposta ao cliente cujo código foi enviado")
+
+	@ApiResponses(value= {
+		@ApiResponse(
+			code=200,
+			message="Retorna a proposta inserida",
+			response=Propostas.class),
+		@ApiResponse(
+			code=500,
+			message="Retorna um erro 500 caso algum erro seja encontrado",
+			response=InternalError.class)
+		})
 	@PostMapping("/{cliente}/propostas")
 	public Propostas setAnalise(@PathVariable Integer cliente) {
 		return propostaService.proposta(cliente);
 	}
 	
+	@ApiOperation(
+			value="Lista todas propostas do cliente",
+			response=List.class,
+			notes="Retorna todas as propostas do cliente cujo código foi enviado")
+
+	@ApiResponses(value= {
+		@ApiResponse(
+			code=200,
+			message="Retorna uma lista de propostas",
+			response=List.class),
+		@ApiResponse(
+			code=404,
+			message="Retorna um erro 404 caso o cliente não tenha nenhuma proposta",
+			response=NotFoundException.class)
+		})
 	@GetMapping("/{cliente}/propostas")
 	public List<Propostas> getAnalises(@PathVariable Integer cliente) {		
 		return propostaService.getPropostaByCliente(clientesService.getCliente(cliente));
 	}
 	 
+	@ApiOperation(
+			value="Busca uma proposta do cliente",
+			response=Propostas.class,
+			notes="Retorna uma proposta do cliente de acordo com os códigos enviados")
+
+	@ApiResponses(value= {
+		@ApiResponse(
+			code=200,
+			message="Retorna uma proposta",
+			response=Propostas.class),
+		@ApiResponse(
+			code=404,
+			message="Retorna um erro 404 caso a proposta não seja encontrada",
+			response=NotFoundException.class)
+		})
 	@GetMapping("/{cliente}/propostas/{codigo}")
 	public Propostas getAnalise(@PathVariable Integer cliente, @PathVariable Integer codigo) {
 		return propostaService.getProposta(cliente, codigo);
